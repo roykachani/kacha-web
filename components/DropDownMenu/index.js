3a61fb244c4121bb49cd522cb98caa01346e27f1
Roy Kachani <roy.kachani@gmail.com>
2021-10-07 15:49:39 -0300

    se agrego link a cv en .pdf

--- components/DropDownMenu/index.js
+++ components/DropDownMenu/index.js
@@ -3,6 +3,12 @@ import styles from './styles.module.css';
 
 export const DropDownMenu = () => {
 	const DropDownItem = (props) => {
+		if (props.children == 'Resume')
+			return (
+				<Link href={'cv/Roy_kachani_octubre2021.pdf'}>
+					<a className={styles.menu_item}>{props.children}</a>
+				</Link>
+			);
 		return (
 			<Link href={`/${props.children.toLowerCase()}`}>
 				<a className={styles.menu_item}>{props.children}</a>
