7de17fdbfcc8bcca8ab028c14519eafed2ab9c23
Roy Kachani <roy.kachani@gmail.com>
2022-06-09 17:56:05 -0300

    add contact component

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { ContactForm } from '../components/ContactForm';
 import { Footer } from '../components/Footer';
 import { Nav } from '../components/Nav';
 import styles from '../styles/Contact.module.css';
@@ -15,7 +16,9 @@ export default function Contact({ open, setOpen }) {
         <div className={styles.contact_title}>
           <h1>Contact me</h1>
         </div>
-        <div className={styles.contact}>{/* <ContactForm /> */}</div>
+        <div className={styles.contact}>
+          <ContactForm />
+        </div>
       </div>
       <Footer />
     </>
