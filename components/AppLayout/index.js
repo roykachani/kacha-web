edb55752d23278954a511325ed2bfe9d98666a9c
Roy Kachani <roy.kachani@gmail.com>
2022-06-09 15:05:58 -0300

    new img

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -3,65 +3,65 @@ import Head from 'next/head';
 import styles, { globalStyles } from './styles';
 
 export const AppLayout = ({ children, open, setOpen }) => {
-	const handleMenu = () => {
-		if (open) setOpen(false);
-	};
-	return (
-		<>
-			<Head>
-				<title>Roy Kachani | Software Developer</title>
-				<meta name="title" content="Roy kachani | Software Developer" />
-				<meta
-					name="description"
-					content="I'm Roy Kachani a 30 years old Software developer born in Buenos Aires, Argentina."
-				/>
-				<meta content="Roy Kachani" name="copyright" />
-				<meta
-					name="keywords"
-					content="Software, Developer, Programmer, Web, Fullstack, Frontend, Backend"
-				/>
-				<link rel="canonical" href="https://roykachani.com/" />
-				<meta
-					name="google-site-verification"
-					content="U2v0SLdeGI6pcJaEP4RlwSe_2rtN40IT6e0I6hHRkx0"
-				/>
-				<meta content="#fff" name="theme-color" />
-				<meta property="og:title" content="Roy Kachani" />
-				<meta property="og:site_name" content="Roy Kachani" />
-				<meta name="author" content="Roy Kachani" />
-				<meta property="og:url" content="https://roykachani.com/" />
-				<meta
-					property="og:description"
-					content="I'm Roy Kachani a 31 years old Software developer born in Buenos Aires, Argentina."
-				/>
-				<meta property="og:image" content="https://roykachani.com/img.jpeg" />
+  const handleMenu = () => {
+    if (open) setOpen(false);
+  };
+  return (
+    <>
+      <Head>
+        <title>Roy Kachani | Software Developer</title>
+        <meta name="title" content="Roy kachani | Software Developer" />
+        <meta
+          name="description"
+          content="I'm Roy Kachani a 30 years old Software developer born in Buenos Aires, Argentina."
+        />
+        <meta content="Roy Kachani" name="copyright" />
+        <meta
+          name="keywords"
+          content="Software, Developer, Programmer, Web, Fullstack, Frontend, Backend"
+        />
+        <link rel="canonical" href="https://roykachani.com/" />
+        <meta
+          name="google-site-verification"
+          content="U2v0SLdeGI6pcJaEP4RlwSe_2rtN40IT6e0I6hHRkx0"
+        />
+        <meta content="#fff" name="theme-color" />
+        <meta property="og:title" content="Roy Kachani" />
+        <meta property="og:site_name" content="Roy Kachani" />
+        <meta name="author" content="Roy Kachani" />
+        <meta property="og:url" content="https://roykachani.com/" />
+        <meta
+          property="og:description"
+          content="I'm Roy Kachani a 31 years old Software developer born in Buenos Aires, Argentina."
+        />
+        <meta property="og:image" content="https://roykachani.com/me1.jpeg" />
 
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:site" value="@roykachani" />
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link
-					href="https://fonts.cdnfonts.com/css/itc-avant-garde-pro-md"
-					rel="preload"
-				/>
-			</Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" value="@roykachani" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          href="https://fonts.cdnfonts.com/css/itc-avant-garde-pro-md"
+          rel="preload"
+        />
+      </Head>
 
-			<main onClick={handleMenu}>{children}</main>
+      <main onClick={handleMenu}>{children}</main>
 
-			<style jsx>{styles}</style>
-			<style jsx global>
-				{globalStyles}
-			</style>
-		</>
-	);
+      <style jsx>{styles}</style>
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </>
+  );
 };
--- components/Nav/index.js
+++ components/Nav/index.js
@@ -4,44 +4,44 @@ import { DropDownMenu } from '../DropDownMenu';
 
 import styles from './styles.module.css';
 export const Nav = ({ open, setOpen }) => {
-	return (
-		<>
-			<header className={styles.header}>
-				<div className={styles.nav}>
-					<div className={styles.nav_items}>
-						<div className={styles.box_logo}>
-							<Link href="/">
-								<a>
-									<img
-										className={styles.logo}
-										src="/logo_Roy.png"
-										alt="logo image"
-									/>
-								</a>
-							</Link>
-						</div>
-						<div className={styles.avatar}>
-							<button
-								type="button"
-								className={styles.button}
-								onClick={() => setOpen(!open)}
-							>
-								<img
-									loading="lazy"
-									className={styles.img}
-									alt="Roy-img"
-									src="/img.jpeg"
-								/>
-							</button>
-							{!!open && (
-								<div className={styles.dropDownMenu_container}>
-									<DropDownMenu />
-								</div>
-							)}
-						</div>
-					</div>
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.nav}>
+          <div className={styles.nav_items}>
+            <div className={styles.box_logo}>
+              <Link href="/">
+                <a>
+                  <img
+                    className={styles.logo}
+                    src="/logo_Roy.png"
+                    alt="logo image"
+                  />
+                </a>
+              </Link>
+            </div>
+            <div className={styles.avatar}>
+              <button
+                type="button"
+                className={styles.button}
+                onClick={() => setOpen(!open)}
+              >
+                <img
+                  loading="lazy"
+                  className={styles.img}
+                  alt="Roy-img"
+                  src="/me1.jpg"
+                />
+              </button>
+              {!!open && (
+                <div className={styles.dropDownMenu_container}>
+                  <DropDownMenu />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
