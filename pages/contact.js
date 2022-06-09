d5431534b45c71ea2bdc2d49e477c366e5aa3bf4
Roy Kachani <roy.kachani@gmail.com>
2022-06-09 17:32:25 -0300

    fix module

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,26 @@
 import Head from 'next/head';
 
-import { ContactForm } from '../components/ContactForm';
+import { ContactForm } from '../components/ContactForm/index';
 import { Footer } from '../components/Footer';
 import { Nav } from '../components/Nav';
 import styles from '../styles/Contact.module.css';
 
 export default function Contact({ open, setOpen }) {
-	return (
-		<>
-			<Head>
-				<title>Contact | Roy Kachani</title>
-			</Head>
-			<Nav open={open} setOpen={setOpen} />
-			<div className={styles.contact_container}>
-				<div className={styles.contact_title}>
-					<h1>Contact me</h1>
-				</div>
-				<div className={styles.contact}>
-					<ContactForm />
-				</div>
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Contact | Roy Kachani</title>
+      </Head>
+      <Nav open={open} setOpen={setOpen} />
+      <div className={styles.contact_container}>
+        <div className={styles.contact_title}>
+          <h1>Contact me</h1>
+        </div>
+        <div className={styles.contact}>
+          <ContactForm />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
