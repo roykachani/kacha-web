29653f4995d0e3a6cfceb3174310eda01d23b139
Roy Kachani <roy.kachani@gmail.com>
2022-06-13 15:54:40 -0300

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,30 @@
 import { useState } from 'react';
+import Script from 'next/script';
 
 import { AppLayout } from '../components/AppLayout';
 
 function MyApp({ Component, pageProps }) {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	return (
-		<AppLayout open={open} setOpen={setOpen}>
-			<Component {...pageProps} open={open} setOpen={setOpen} />
-		</AppLayout>
-	);
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletamanager.com/gtag/js?id=G-P0T80701XL"
+      />
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-P0T80701XL');
+		`}
+      </Script>
+      <AppLayout open={open} setOpen={setOpen}>
+        <Component {...pageProps} open={open} setOpen={setOpen} />
+      </AppLayout>
+    </>
+  );
 }
 
 export default MyApp;
